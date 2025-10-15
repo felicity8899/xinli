780020af90c6d08ae3c686fbc0b3c8bcea514932
felicity8899 <xinlizhang25@gmail.com>
2025-10-14 22:34:28 -0400

    Create display.py

---
+++ wqu/display.py
@@ -0,0 +1,122 @@
+from business import GraphBuilder, StatsBuilder
+from dash import Input, Output, State, dcc, html, Dash
+
+
+# Task 7.4.1
+app = Dash(__name__)
+# Task 7.4.8
+gb = GraphBuilder()
+# Task 7.4.13
+sb = ...
+
+# Tasks 7.4.1, 7.4.2, 7.4.3, 7.4.11, 7.4.14, 7.4.16
+app.layout = html.Div([
+    html.H1("Application Demographics"),
+    dcc.Dropdown(
+        options=["Nationality", "Age", "Education"],
+        value="Nationality",
+        id="demo-plots-dropdown"
+    ),
+    html.Div(id="demo-plot-display"),
+    html.H1("Experiment"),
+    html.H2("Choose your effect size"),
+    dcc.Slider(min=0.1, max=0.8, step=0.1, value=0.2, id="effect-size-slider"),
+    html.Div(id="effect-size-display"),
+    html.H1("Results"),
+])
+
+
+# Tasks 7.4.4, 7.4.8, 7.4.9, 7.4.10
+@app.callback(
+    Output("demo-plot-display", "children"),
+    Input("demo-plots-dropdown", "value")
+)
+def display_demo_graph(graph_name):
+    """Serves applicant demograhic visualization.
+
+    Parameters
+    ----------
+    graph_name : str
+        User input given via 'demo-plots-dropdown'. Name of Graph to be returned.
+        Options are 'Nationality', 'Age', 'Education'.
+
+    Returns
+    -------
+    dcc.Graph
+        Plot that will be displayed in 'demo-plots-display' Div.
+    """
+    if graph_name == "Nationality":
+        fig = gb.build_nat_choropleth()
+    elif graph_name == "Age":
+        fig = gb.build_age_hist()
+    else:
+        fig = gb.build_ed_bar()
+    return dcc.Graph(figure=fig)
+
+
+# Task 7.4.13
+
+def display_group_size(effect_size):
+    """Serves information about required group size.
+
+    Parameters
+    ----------
+    effect_size : float
+        Size of effect that user wants to detect. Provided via 'effect-size-slider'.
+
+    Returns
+    -------
+    html.Div
+        Text with information about required group size. will be displayed in
+        'effect-size-display'.
+    """
+
+    return html.Div()
+
+
+# Task 7.4.16
+
+def display_cdf_pct(effect_size, days):
+    """Serves probability of getting desired number of obervations.
+
+    Parameters
+    ----------
+    effect_size : float
+        The effect size that user wants to detect. Provided via 'effect-size-slider'.
+    days : int
+        Duration of the experiment. Provided via 'experiment-days-slider'.
+
+    Returns
+    -------
+    html.Div
+        Text with information about probabilty. Goes to 'experiment-days-display'.
+    """
+    # Calculate number of observations
+
+    # Calculate percentage
+
+    # Create text
+   
+    # Return Div with text
+    return html.Div()
+
+
+# Task 7.4.18
+
+def display_results(n_clicks, days):
+    """Serves results from experiment.
+
+    Parameters
+    ----------
+    n_clicks : int
+        Number of times 'start-experiment-button' button has been pressed.
+    days : int
+        Duration of the experiment. Provided via 'experiment-days-display'.
+
+    Returns
+    -------
+    html.Div
+        Experiment results. Goes to 'results-display'.
+    """
+   
+    return html.Div()
